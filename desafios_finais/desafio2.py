6eb86e894a06ffe6328017c0b211f1b2919c9201
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2026-03-20 13:33:25 -0300

    desafio2.py add

---
+++ desafios_finais/desafio2.py
@@ -0,0 +1,67 @@
+import random
+
+def gerar_baralho(n_copias=2, coringas=True, embaralhado=True):
+    baralho = []
+
+
+    naipes = list('♥️♦️♣️♠️')
+    numeros = list('A23456789') + ['10'] + list('JQK')
+
+    for _ in range(n_copias):
+        for naipe in naipes:
+            for numero in numeros:
+                carta = numero + naipe
+                baralho.append(carta)
+
+    if coringas:
+        baralho.append('JK1')
+        baralho.append('JK2')
+
+
+    for naipe in naipes:
+        for numero in numeros:
+            carta = numero + naipe
+            baralho.append(carta)
+
+    if coringas:
+        baralho.append('JK1')
+        baralho.append('JK2')
+    
+    if embaralhado:
+        random.shuffle(baralho)
+    
+    return baralho
+
+def mostrar_baralho(baralho):
+    print(f'Há {len(baralho)} cartas no baralho:')
+    print('Cartas:')
+    print(' ,  '.join(baralho))
+
+
+def dar_as_cartas(baralho, n_jogadores=4, n_cartas=5):
+    jogadores = {}
+
+    for i in range(n_jogadores):
+        mao = []
+        while len(mao) < n_cartas:
+            carta = baralho.pop(0)
+            mao.append(carta)
+        nome_jogador = f'Jogador {i+1}'
+        jogadores[nome_jogador] = mao
+
+    return jogadores
+
+def mostrar_jogadores(jogadores):
+    for jogador, mao in jogadores.items():
+        print(f'Há {len(mao)} cartas na mão de {jogador}:')
+        print('Cartas:')
+        for carta in mao:
+            print(carta)
+
+
+
+baralho = gerar_baralho()
+mostrar_baralho(baralho)
+
+jogadores = dar_as_cartas(baralho)
+mostrar_jogadores(jogadores)
